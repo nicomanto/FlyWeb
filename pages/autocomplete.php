5d64a6469ef7da541d9327c73d97188f88fce29d
xi <perinellolor@gmail.com>
2020-11-09 15:08:25 +0100

    form completato (funzione inserimento tag con autocomplete fatta), riguardare accessibilità. procedo con creazione back-end per inserimento in db dei dati raccolti e js per controlli form. consulto sulle immagini inserite richiesto:

--- pages/autocomplete.php
+++ pages/autocomplete.php
@@ -7,35 +7,24 @@ if($link){
      echo "ok";
 }else{
      echo "mannaggia gesi";
-}
+}*/
 
- echo $link->host_info . ", kek";
 
-*/
 
- if(isset($_POST["query"]))  
- { 
       $output = '';  
-      $query = "SELECT * FROM Tag WHERE Nome LIKE '%".$_POST["query"]."%'";  
-      $result = mysqli_query($connect, $query);  
-      echo $result;
-      $output = '<ul class="list-unstyled">';  
+      $query = "SELECT * FROM Tag ";  
+      $result = mysqli_query($link, $query);  
       if(mysqli_num_rows($result) > 0)  
       {  
            while($row = mysqli_fetch_array($result))  
            {  
-                $output .= '<li>'.$row["Nome"].'</li>';  
+                $output .= ' <option value='.$row["Nome"].'>'.$row["Nome"].'</option> ';  
            }  
       }  
       else  
       {  
-           $output .= '<li>nessun tag del genere</li>';  
+           $output .= ' nessun tag del genere ';  
       }  
-      $output .= '</ul>';  
+      $output .= '';
       echo $output;  
- }else{
-      echo "fallito";
- }
-
-
  ?> 
