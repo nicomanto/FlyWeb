6a526347ec9d892f904f8671e45a7e56845799f0
Niccolò Mantovani <nicomanto49@gmail.com>
2020-11-14 17:32:18 +0100

    Remove test file

--- flyweb/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once($_SERVER['DOCUMENT_ROOT'] . 'autoload.php');
-
-    $travelController = new \controllers\TravelController(12);
-
-    // Loading signup template
-    $page = (new html\components\TravelReviews($travelController->getTravelReviewsList()));
-
-    echo $page;
