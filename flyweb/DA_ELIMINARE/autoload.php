d23afa35625e5b4a9e28ef0c3dd9f31857014079
Filippo <pinton.filippo@protonmail.com>
2020-11-27 17:42:29 +0100

    Fixed htppd config

--- flyweb/DA_ELIMINARE/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Start session
-    session_start();
-
-    // Register autoload function
-    spl_autoload_register(function ($className) {
-        $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
-        require_once($className . '.php');
-    });
