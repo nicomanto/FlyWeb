5165630045f21d753279dfe038b08d7fc097799f
Niccolò Mantovani <nicomanto49@gmail.com>
2020-12-06 22:01:57 +0000

    Feature/page about us

--- flyweb/model/Menu.php
+++ flyweb/model/Menu.php
@@ -8,7 +8,7 @@
         function __construct() {
             $this->MenuItem= array (
                 "home"  => new MenuItem("index.php","Home",array("NotLoggedUser", "LogAmministratore","LoggedUser")),
-                "aboutUs" => new MenuItem("aboutus.php","About us",array("NotLoggedUser", "LogAmministratore","LoggedUser")),
+                "aboutUs" => new MenuItem("aboutUs.php","About us",array("NotLoggedUser", "LogAmministratore","LoggedUser")),
                 "profilo"   => new MenuItem("profilo.php","Profilo",array("LogAmministratore","LoggedUser")),
                 "carrello" => new MenuItem("carrello.php","Carrello",array("LoggedUser")),
                 "login" => new MenuItem("login.php","Accedi",array("NotLoggedUser")),
