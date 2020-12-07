647788050416036b02c436916a9791101be42854
Filippo <pinton.filippo@protonmail.com>
2020-12-07 19:54:03 +0100

    Removed useless path part

--- flyweb/model/AdmMenu.php
+++ flyweb/model/AdmMenu.php
@@ -6,11 +6,11 @@
         function __construct() {
             $this->MenuItem= array (
 
-                "inserisci_viaggio" => new MenuItem("admin/form_inserimento.php","Iserisci viaggio",array("LoggedAdmin")),
-                "gestisci_viaggi"   => new MenuItem("admin/search.php","Gestisci viaggi",array("LoggedAdmin")),
-                "inserisci_integrazione" => new MenuItem("admin/integrazione_inserimento.php","Inserisci integrazione",array("LoggedAdmin")),
-                "gestisci_integrazioni" => new MenuItem("admin/integrazioni.php","Gestisci integrazioni",array("LoggedAdmin")),
-                "modera_recensioni" => new MenuItem("admin/moderazione_recensioni.php","Modera recensioni",array("LoggedAdmin")),
+                "inserisci_viaggio" => new MenuItem("form_inserimento.php","Iserisci viaggio",array("LoggedAdmin")),
+                "gestisci_viaggi"   => new MenuItem("search.php","Gestisci viaggi",array("LoggedAdmin")),
+                "inserisci_integrazione" => new MenuItem("integrazione_inserimento.php","Inserisci integrazione",array("LoggedAdmin")),
+                "gestisci_integrazioni" => new MenuItem("integrazioni.php","Gestisci integrazioni",array("LoggedAdmin")),
+                "modera_recensioni" => new MenuItem("moderazione_recensioni.php","Modera recensioni",array("LoggedAdmin")),
                 "logout" => new MenuItem("logout.php","Logout",array("LoggedAdmin"))
             );    
         }
