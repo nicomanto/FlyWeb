0b60bedd06d3f3a6f7eebacf55602f5223bcf8e9
Filippo <pinton.filippo@protonmail.com>
2020-12-07 16:55:26 +0100

    Updated exit

--- flyweb/controllers/RouteController.php
+++ flyweb/controllers/RouteController.php
@@ -7,30 +7,31 @@ class RouteController {
     public static function protectedRoute(): void {
         if (!$_SESSION['admin'] || !$_SESSION['logged_in']) {
             header('location:/index.php');
-            exit;
+            exit();
         }
     }
 
     public static function unprotectedRoute(): void {
         if ($_SESSION['admin']) {
             header('location:/admin/index.php');
+            exit();
         }
     }
 
     public static function unloggedRoute(): void {
         if ($_SESSION['logged_in'] && $_SESSION['admin']) {
             header('location:/admin/index.php');
-            exit;
+            exit();
         } else if ($_SESSION['logged_in']) {
             header('location:/index.php');
-            exit;
+            exit();
         }
     }
 
     public static function loggedRoute(): void {
         if (!$_SESSION['logged_in']) {
             header('location:/index.php');
-            exit;
+            exit();
         }
     }
 
