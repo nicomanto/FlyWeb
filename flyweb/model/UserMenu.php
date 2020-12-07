d6dc7c723c94e1df60dfc7dc0c325576f74f2c35
Filippo <pinton.filippo@protonmail.com>
2020-12-07 19:21:12 +0100

    Updated paths

--- flyweb/model/UserMenu.php
+++ flyweb/model/UserMenu.php
@@ -5,13 +5,13 @@
         
         function __construct() {
             $this->MenuItem= array (
-                "home"  => new MenuItem("/index.php","Home",array("NotLoggedUser", "LoggedAdmin","LoggedUser"),"en"),
-                "aboutUs" => new MenuItem("/aboutUs.php","About us",array("NotLoggedUser", "LoggedAdmin","LoggedUser"),"en"),
-                "profilo"   => new MenuItem("/profilo.php","Profilo",array("LoggedAdmin","LoggedUser")),
-                "carrello" => new MenuItem("/carrello.php","Carrello",array("LoggedUser")),
-                "login" => new MenuItem("/login.php","Accedi",array("NotLoggedUser")),
-                "logOut" => new MenuItem("/logout.php","Esci",array("LoggedAdmin","LoggedUser")),
-                "signUp" => new MenuItem("/signup.php","Registrati",array("NotLoggedUser"))
+                "home"  => new MenuItem("index.php","Home",array("NotLoggedUser", "LoggedAdmin","LoggedUser"),"en"),
+                "aboutUs" => new MenuItem("aboutUs.php","About us",array("NotLoggedUser", "LoggedAdmin","LoggedUser"),"en"),
+                "profilo"   => new MenuItem("profilo.php","Profilo",array("LoggedAdmin","LoggedUser")),
+                "carrello" => new MenuItem("carrello.php","Carrello",array("LoggedUser")),
+                "login" => new MenuItem("login.php","Accedi",array("NotLoggedUser")),
+                "logOut" => new MenuItem("logout.php","Esci",array("LoggedAdmin","LoggedUser")),
+                "signUp" => new MenuItem("signup.php","Registrati",array("NotLoggedUser"))
             );    
         }
     }
