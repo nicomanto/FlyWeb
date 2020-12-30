b9b5e8d85d3cc814b07cc476de1163fe99f9214f
Silvia Giro <girosilvia2399@gmail.com>
2020-12-30 12:13:53 +0100

    Sistemato roba

--- flyweb/js/formUtenteScript.js
+++ flyweb/js/formUtenteScript.js
@@ -2,12 +2,4 @@ function clicked(){
     $popup_conferma = confirm("vuoi davvero davvero eliminare il tuo profilo? :( :(");
     return ($popup_conferma)?true:false;
 }
-function showPwd() {
-    var input = document.getElementById('password');
-    if (input.type === "password") {
-      input.type = "text";
-    } else {
-      input.type = "password";
-    }
-  }
 
