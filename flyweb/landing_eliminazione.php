c413996f9b50ab2c5988bcd7bc2747b679d481c2
Silvia Giro <girosilvia2399@gmail.com>
2021-01-05 13:06:54 +0100

    landing eliminazione

---
+++ flyweb/landing_eliminazione.php
@@ -0,0 +1,31 @@
+<?php
+
+use controllers\UserController;
+
+require_once($_SERVER['DOCUMENT_ROOT'] . 'autoload.php');
+
+    extract($_POST, EXTR_SKIP);
+
+    $page = new \html\template('modifica_info_profilo');
+
+    $page->replaceTag('HEAD', (new \html\components\head));
+    // Set nav menu
+    $page->replaceTag('NAV-MENU', (new \html\components\PrincipalMenu));
+
+    $breadcrumb=array(
+        new model\BreadcrumbItem("/datipersonali.php","Profilo"),
+        new model\BreadcrumbItem("/modifica_info_profilo.php","Modifica profilo"),
+        new model\BreadcrumbItem("#","Conferma eliminazione")
+    );
+
+    $page->replaceTag('BREADCRUMB', (new \html\components\Breadcrumb($breadcrumb)));
+
+    $page->replaceTag('PROFILOMENU', (new \html\components\ProfiloMenu));
+
+    $page->replaceTag('SUCCESSO-MODIFICA', "");
+
+    $page->replaceTag('ELIMINAZIONE', (new \html\components\EliminazioneProfilo()));
+
+    $page->replaceTag('FOOTER', (new \html\components\footer));
+
+    echo $page;
