489fb444403b886eb793ede4d734db20d96ae1bd
Niccolò Mantovani <nicomanto49@gmail.com>
2021-01-06 08:33:01 +0100

    FLYW-115 - Eliminazione integrazione e spostati file per le integrazioni

--- flyweb/model/AdmMenu.php
+++ flyweb/model/AdmMenu.php
@@ -8,8 +8,6 @@
                 "home" => new MenuItem("/admin/index.php","Home",array("LoggedAdmin"),"en"),
                 "inserisci_viaggio" => new MenuItem("/admin/form_inserimento.php","Inserisci viaggio",array("LoggedAdmin")),
                 "gestisci_viaggi"   => new MenuItem("/admin/search.php","Gestisci viaggi",array("LoggedAdmin")),
-                "inserisci_integrazione" => new MenuItem("/admin/integrazione_inserimento.php","Inserisci integrazione",array("LoggedAdmin")),
-                "gestisci_integrazioni" => new MenuItem("/admin/integrazioni.php","Gestisci integrazioni",array("LoggedAdmin")),
                 "modera_recensioni" => new MenuItem("/admin/moderazione_recensioni.php","Modera recensioni",array("LoggedAdmin")),
                 "logout" => new MenuItem("/logout.php","Logout",array("LoggedAdmin"))
             );    
